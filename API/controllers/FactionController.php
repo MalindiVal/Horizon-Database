e4864d2b16215ae52025ca5c61e8980dfc4002a6
Valentin Malindi <115624045+iutdijonMalindi@users.noreply.github.com>
2025-05-12 12:26:34 +0200

    Ajout des controleurs

---
+++ API/controllers/FactionController.php
@@ -0,0 +1,17 @@
+<?php
+require_once("../models/FactionManager.php");
+class FactionController{
+
+    private FactionManager $manager;
+    public function __construct() {
+        $this->manager = new FactionManager();
+    }
+
+    public function getAll(){
+        echo json_encode($this->manager->getAll());
+    }
+
+    public function getById(int $id){
+        echo json_encode($this->manager->getByID($id));
+    }
+}
---
+++ API/controllers/RaceController.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../models/RaceManager.php");
+require_once("../models/Race.php");
+class RaceController{
+
+    private RaceManager $manager;
+    public function __construct() {
+        $this->manager = new RaceManager();
+    }
+
+    public function getAll(){
+        echo json_encode($this->manager->getAll());
+    }
+
+    public function getById(int $id){
+        echo json_encode($this->manager->getByID($id));
+    }
+}
