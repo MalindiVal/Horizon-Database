fbc5d35bcf955ca8032e8c03a8087449965d5a89
Valentin Malindi <115624045+iutdijonMalindi@users.noreply.github.com>
2025-05-13 03:56:16 +0200

    Creation de la classe factionController

--- API/controllers/FactionController.php
+++ API/controllers/FactionController.php
@@ -1,5 +1,6 @@
 <?php
 require_once("./models/FactionManager.php");
+require_once("./models/Faction.php");
 class FactionController{
 
     private FactionManager $manager;
@@ -13,5 +14,25 @@ class FactionController{
 
     public function getById(int $id){
         echo json_encode($this->manager->getByID($id));
+    }
+
+    public function Add(Faction $perso){
+        try {
+            $res = $this->manager->AddFaction($perso);
+            echo json_encode($res > 0);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(["error" => $e->getMessage()]);
+        }
+    }
+
+    public function Update(Faction $perso){
+        try {
+            $this->manager->UpdateFaction($perso);
+            http_response_code(200);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(["error" => $e->getMessage()]);
+        }
     }
 }
