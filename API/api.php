ed3c6fbf7f1a12a2b7829372626f484ceea384bd
Valentin Malindi <115624045+iutdijonMalindi@users.noreply.github.com>
2025-05-13 02:38:22 +0200

    Ajout du routeur dans le fichier principal

---
+++ API/api.php
@@ -0,0 +1,22 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *"); // Corrigé ici
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE"); // Corrigé ici
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Vérifier si c'est une requête OPTIONS (pré-vérification)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+require_once("controllers/Router/Router.php");
+
+$router = new Router();
+
+// Route the request
+$router->routing($_GET,$_POST);
+
+?>
+
+
