2a31f09a02acdb00e5a3d9eace8876f5cbe3d530
Valentin Malindi <115624045+iutdijonMalindi@users.noreply.github.com>
2025-05-13 02:40:34 +0200

    Amélioration du routeur

--- API/controllers/Router/Router.php
+++ API/controllers/Router/Router.php
@@ -1,5 +1,11 @@
 <?php
-require_once("Route/RouteIndex.php");
+require_once("Route/RouteGetAllPersonnages.php");
+require_once("Route/RouteGetByIdPersonnage.php");
+require_once("Route/RouteAddPersonnage.php");
+require_once("Route/RouteUpdatePersonnage.php");
+require_once("Route/RouteGetAllFactions.php");
+require_once("Route/RouteGetFactionById.php");
+
 require_once("controllers/PersonnageController.php");
 require_once("controllers/FactionController.php");
 require_once("controllers/RaceController.php");
@@ -8,12 +14,15 @@ require_once("controllers/RelationController.php");
 class Router {
     private $routeList;
     private $ctrlList;
-    private $actionKey;
+    private string $actionKey;
 
-    public function __construct($name = "action") {
+    private string $typeKey;
+
+    public function __construct($name = "action", $type = "type") {
         $this->routeList = [];
         $this->ctrlList = [];
         $this->actionKey = $name;
+        $this->typeKey = $type;
         $this->createControllerList();
         $this->createRouteList();
     }
@@ -28,26 +37,36 @@ class Router {
     }
 
     private function createRouteList() {
-        $this->routeList =  [
-            //"index" => new RouteIndex($this->ctrlList["main"]),
-            
+        $this->routeList["personnage"] =  [
+            "get-all" => new RouteGetAllPersonnages($this->ctrlList["personnage"]),
+            "get-by-id" => new RouteGetPersonnagesById($this->ctrlList["personnage"]),
+            "add" => new RouteAddPersonnage($this->ctrlList["personnage"]),
+            "update" => new RouteUpdatePersonnage($this->ctrlList["personnage"]),
         ];
     }
 
-    public function routing($requestData, $post) {
+    public function routing($requestData,$post) {
         
-        $actionKey = !empty($requestData[$this->actionKey]) ? $requestData[$this->actionKey] : null;
+        
+        $typeKey = null;
+        if ( !empty($requestData[$this->actionKey]) ){
+            $typeKey = $requestData[$this->typeKey] ;
+        }
+
+        $actionKey = null;
+        if ( !empty($requestData[$this->actionKey]) ){
+            $actionKey = $requestData[$this->actionKey] ;
+        }
+
         
         if(!empty($post)){
-            $method = 'POST';
-            $this->routeList[$actionKey]->action($post, $method);
+            $this->routeList[$typeKey][$actionKey]->action($post, 'POST');
         }
         else{
-            $method = 'GET';
-            if($actionKey == null)
-                $this->routeList["index"]->action($requestData, 'GET');
-            else{
-                $this->routeList[$actionKey]->action($requestData, $method);
+            if($actionKey != null){
+                $this->routeList[$typeKey][$actionKey]->action($requestData, 'GET');
+            } else {
+                $this->routeList[$typeKey][""]->action($requestData, 'GET');
             }
         }
     }
