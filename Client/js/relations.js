3f596ad2c09e2f85966831d7984ff01ce01c4a13
Valentin Malindi <115624045+iutdijonMalindi@users.noreply.github.com>
2025-05-27 17:46:38 +0200

    Ajoute de la mise en place de la vue des relations

--- Client/js/relations.js
+++ Client/js/relations.js
@@ -1,1 +1,6 @@
+window.onload = function () {
+    let dao = new RelationDAO();
+    let ctrl = new RelationController(dao);
+    let view = new RelationsView(ctrl);
+};
 //# sourceMappingURL=relations.js.map
