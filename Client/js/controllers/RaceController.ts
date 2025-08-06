513f6ffa719367862108759e1a5616cf18712457
Valentin Malindi <115624045+iutdijonMalindi@users.noreply.github.com>
2025-08-06 23:00:14 +0200

    Correction du race controller

--- Client/js/controllers/RaceController.ts
+++ Client/js/controllers/RaceController.ts
@@ -13,7 +13,7 @@ class RaceController extends Observable{
         {
             let response = await this.dao.GetAll();
             response.forEach( element => {
-                this.NotifyAjoutRace(element);
+                this.NotifyRaceFound(element);
                 list.push(element);
             });
         } catch {
